47e0a37915244312c55b6b8eb4a3a62a2d03776c
Kyle Ko <kyleko1234@gmail.com>
2020-05-11 10:50:26 -0700

    Put mapping for Person implemented

--- rest_api/src/main/java/com/easyledger/api/PersonController.java
+++ rest_api/src/main/java/com/easyledger/api/PersonController.java
@@ -2,9 +2,13 @@ package com.easyledger.api;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,17 +26,33 @@ public class PersonController {
 
 
 	@GetMapping("/person")
-    public List<Person> getAllLineItems() {
+    public List<Person> getAllPersons() {
         return personRepo.findAll();
     }
 
 	
     @GetMapping("/person/{id}")
-    public ResponseEntity<Person> getLineItemById(@PathVariable(value = "id") Long personId)
+    public ResponseEntity<Person> getPersonId(@PathVariable(value = "id") Long personId)
         throws ResourceNotFoundException {
     	Person person = personRepo.findById(personId)
     		.orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + personId)); 
         return ResponseEntity.ok().body(person);
     }
+    
+    @PutMapping("/person/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable(value = "id") Long personId,
+    			@Valid @RequestBody Person personDetails) throws ResourceNotFoundException {
+    	Person person = personRepo.findById(personId)
+    		.orElseThrow(() -> new ResourceNotFoundException("Person not found for this id :: " + personId));
+    	person.setFirstName(personDetails.getFirstName());
+    	person.setLastName(personDetails.getLastName());
+    	person.setEmail(personDetails.getEmail());
+    	person.setPassword(personDetails.getPassword());
+    	final Person updatedPerson = personRepo.save(person);
+    	return ResponseEntity.ok(updatedPerson);
+    				
+    }
+    	
+
 }
 
