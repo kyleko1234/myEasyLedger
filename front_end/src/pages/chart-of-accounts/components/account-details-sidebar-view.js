a15f015e0ce6a81036d6f9f6483044002bae5d29
Kyle Ko <kyleko1234@gmail.com>
2020-10-18 18:30:55 -0700

    added edit and delete buttons to account details sidebar. currently not functional, but delete button will toggle an alert to check if the user is sure they want to delete. on confirmation nothing happens yet, but if the account is not empty a danger alert is shown and the operation is cancelled

--- front_end/src/pages/chart-of-accounts/components/account-details-sidebar-view.js
+++ front_end/src/pages/chart-of-accounts/components/account-details-sidebar-view.js
@@ -9,25 +9,46 @@ function AccountDetailsSidebarView( props ) {
 
     return (
         <div>
-            <p> <strong>{"Account Name: "}</strong> {props.accountName} </p> 
-            <p> 
-                <strong>{"Account Subtype: "}</strong>
-                {props.accountSubtypes ? 
-                    props.accountSubtypes.slice()
-                        .find(accountSubtype => accountSubtype.accountSubtypeId.toString() === props.accountSubtypeId.toString()).accountSubtypeName
-                : "Loading..."} 
-            </p>
-            <p> 
-                <strong>{"Account Type: "}</strong>
-                {props.accountTypes ? 
-                    props.accountTypes.slice()
-                        .find(accountType => accountType.id.toString() === props.accountTypeId.toString()).name
-                : "Loading..."} 
-            </p>
-            <p> <strong>{"Total Debits: "}</strong> {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.debitTotal)} </p> 
-            <p> <strong>{"Total Credits: "}</strong> {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.creditTotal)} </p> 
-            <p> <strong>{"Balance: "}</strong> {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.debitTotal - props.creditTotal)} </p> 
-
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Account Name: "}</span>
+                <span className="col-md-6"> {props.accountName} </span> 
+            </div> 
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Account Subtype: "}</span>
+                <span className="col-md-6">
+                    {props.accountSubtypes ? 
+                        props.accountSubtypes.slice()
+                            .find(accountSubtype => accountSubtype.accountSubtypeId.toString() === props.accountSubtypeId.toString()).accountSubtypeName
+                    : "Loading..."} 
+                </span>
+            </div>
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Account Type: "}</span>
+                <span className="col-md-6">
+                    {props.accountTypes ? 
+                        props.accountTypes.slice()
+                            .find(accountType => accountType.id.toString() === props.accountTypeId.toString()).name
+                    : "Loading..."} 
+                </span>
+            </div>
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Total Debits: "}</span>
+                <span className="col-md-6">
+                     {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.debitTotal)}
+                </span>
+            </div> 
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Total Debits: "}</span>
+                <span className="col-md-6">
+                     {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.creditTotal)}
+                </span>
+            </div>             
+            <div className="row my-3">
+                <span className="col-md-6 font-weight-bold">{"Total Balance: "}</span>
+                <span className="col-md-6">
+                     {new Intl.NumberFormat(props.context.localization.locale, { style: 'currency', currency: props.context.localization.currency }).format(props.debitTotal - props.creditTotal)}
+                </span>
+            </div> 
 
         </div>
     )
--- front_end/src/pages/chart-of-accounts/components/account-details.js
+++ front_end/src/pages/chart-of-accounts/components/account-details.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ClickableTableWithPaginationAndJournalEntryModal from '../../../components/table/clickable-table-with-pagination-and-journal-entry-modal';
 import axios from 'axios';
-import {Link, useParams} from 'react-router-dom'
-import AccountDetailsSidebarView from "./account-details-sidebar-view"
+import {Link, useParams} from 'react-router-dom';
+import AccountDetailsSidebarView from "./account-details-sidebar-view";
+import SweetAlert from 'react-bootstrap-sweetalert';
+
 
 
 function AccountDetails(props) {
@@ -29,6 +31,15 @@ function AccountDetails(props) {
     const [selectedAccount, setSelectedAccount] = React.useState(null);
     const [accountSubtypes, setAccountSubtypes] = React.useState(null)
     const [accountTypes, setAccountTypes] = React.useState(null);
+
+    const [deleteAccountAlert, setDeleteAccountAlert] = React.useState(false);
+    const toggleDeleteAccountAlert = () => {
+        setDeleteAccountAlert(!deleteAccountAlert);
+    }
+    const [cannotDeleteAccountAlert, setCannotDeleteAccountAlert] = React.useState(false);
+    const toggleCannotDeleteAccountAlert = () => {
+        setCannotDeleteAccountAlert(!cannotDeleteAccountAlert);
+    }
 
     //initially fetch account data, list of subtypes, list of types from API
     React.useEffect(() => {
@@ -67,6 +78,14 @@ function AccountDetails(props) {
             .catch(console.log);
     }, [])
 
+    const handleConfirmDeleteAccountButton = () => {
+        toggleDeleteAccountAlert();
+        if (elementCount != 0) {
+            toggleCannotDeleteAccountAlert();
+        } else {
+            //TODO handle deletion of account
+        }
+    }
     return (
         <>
             <ol className="breadcrumb float-xl-right">
@@ -91,14 +110,54 @@ function AccountDetails(props) {
                     /> : "Loading..."}
                 </span>
                 <span className="col-md-3 border-left">
-                    {selectedAccount ? <AccountDetailsSidebarView
-                        {...selectedAccount}
-                        accountSubtypes={accountSubtypes}
-                        accountTypes={accountTypes}
-                        context={props.context}
-                    /> : "Loading..."}
+                    <div className="row d-flex justify-content-between">
+                        <div>{/* empty div to push the other two buttons to the right */}</div>
+                        <div>
+                            <button 
+                                className="btn btn-default btn-icon btn-lg"
+                            >
+                                <i className="fas fa-edit"></i>
+                            </button>
+                            <button 
+                                className="btn btn-default btn-icon btn-lg"
+                                onClick={() => toggleDeleteAccountAlert()}
+                            >
+                                <i className="fas fa-trash-alt"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div>
+                        {selectedAccount ? <AccountDetailsSidebarView
+                            {...selectedAccount}
+                            accountSubtypes={accountSubtypes}
+                            accountTypes={accountTypes}
+                            context={props.context}
+                        /> : "Loading..."}
+                    </div>
                 </span>
-            </div> 
+            </div>
+
+            {deleteAccountAlert ? 
+                <SweetAlert primary showCancel
+                    confirmBtnText="Yes, delete it!"
+                    confirmBtnBsStyle="primary"
+                    cancelBtnBsStyle="white"
+                    title="Are you sure?"
+                    onConfirm={() => handleConfirmDeleteAccountButton()}
+                    onCancel={() => toggleDeleteAccountAlert()}
+                >
+                    Are you sure you want to delete this account?
+                </SweetAlert> 
+            : null}
+            {cannotDeleteAccountAlert ? 
+                <SweetAlert danger showConfirm={false} showCancel={true}
+                    cancelBtnBsStyle="white"
+                    title="Cannot delete this account."
+                    onCancel={() => toggleCannotDeleteAccountAlert()}
+                >
+                    Please remove all line items from this account and try again.
+                </SweetAlert> 
+            : null}
         </>
     )
 }
