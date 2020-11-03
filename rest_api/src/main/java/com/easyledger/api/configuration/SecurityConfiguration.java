27e167b6daf939506c32409145c1956beddb47be
Kyle Ko <kyleko1234@gmail.com>
2020-11-02 17:00:51 -0800

    implemented rudimentary JWT authentication functionality on backend

--- rest_api/src/main/java/com/easyledger/api/configuration/SecurityConfiguration.java
+++ rest_api/src/main/java/com/easyledger/api/configuration/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.easyledger.api.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.easyledger.api.service.AuthenticationFilter;
+import com.easyledger.api.service.LoginFilter;
 import com.easyledger.api.service.UserDetailsServiceImpl;
 
 @Configuration
@@ -22,6 +32,34 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().and().authorizeRequests()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.addFilterBefore(new LoginFilter("/login", authenticationManager()),
+					UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new AuthenticationFilter(), 
+					UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(true);
+		config.applyPermitDefaultValues();
+		
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+	
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {    
