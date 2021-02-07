ae5e05c5d9d99c7e8723bf5bc9e2271edaac00ba
Kyle Ko <kyleko1234@gmail.com>
2021-02-07 00:13:26 -0800

    resolved conflicts in sidebar-text.js

--- front_end/src/utils/i18n/sidebar-text.js
+++ front_end/src/utils/i18n/sidebar-text.js
@@ -12,16 +12,10 @@ export const sidebarText = { //handles i18n for sidebar, sidebar-profile, sideba
         "Reports": "Reports"
     },
     "zh-TW": {
-<<<<<<< HEAD
-        "Navigation": "TRANSLATION GOES HERE",
         "My EasyLedgers": "TRANSLATION GOES HERE",
-        "Settings": "TRANSLATION GOES HERE",
-        "Sign Out": "TRANSLATION GOES HERE",
-=======
         "Navigation": "網站導覽",
         "Settings": "設定",
         "Sign Out": "登出",
->>>>>>> f4042b8a86a86cd98e414dd5b6ae38a70e773180
 
         //menu items
         "Dashboard": "帳目概覽",
