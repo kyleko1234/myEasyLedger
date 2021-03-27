b4bbdf4b22d01279def54b718ae129e417d2fa11
Kyle Ko <kyleko1234@gmail.com>
2021-03-27 00:35:32 -0700

    Updated AccountDetailsEditor component to include a parent account and a subtype field

--- front_end/src/pages/chart-of-accounts/components/account-details-editor.js
+++ front_end/src/pages/chart-of-accounts/components/account-details-editor.js
@@ -11,17 +11,21 @@ import { useHistory } from 'react-router-dom';
 
 
 function AccountDetailsEditor(props) {
-    //required props: isOpen, toggle, selectedAccountId, fetchData
+    //required props: isOpen, toggle, selectedAccountId, fetchData, createMode,
+    //optional props: accountTypeId
     const appContext = React.useContext(PageSettings);
     const history = useHistory();
 
     const [accountNameInput, setAccountNameInput] = React.useState('');
-    const [accountGroupOptions, setAccountGroupOptions] = React.useState([]);
-    const [selectedAccountGroupId, setSelectedAccountGroupId] = React.useState(null);
+    const [parentAccountOptions, setParentAccountOptions] = React.useState([]);
+    const [selectedParentAccountId, setSelectedParentAccountId] = React.useState(null);
+    const [accountSubtypeOptions, setAccountSubtypeOptions] = React.useState([]);
+    const [selectedAccountSubtypeId, setSelectedAccountSubtypeId] = React.useState(null);
     const [accountTypeId, setAccountTypeId] = React.useState(null);
-    const [initialDebitValueInput, setInitialDebitValueInput] = React.useState(null);
-    const [initialCreditValueInput, setInitialCreditValueInput] = React.useState(null);
+    const [initialDebitValueInput, setInitialDebitValueInput] = React.useState('0');
+    const [initialCreditValueInput, setInitialCreditValueInput] = React.useState('0');
     const [noAccountNameAlert, setNoAccountNameAlert] = React.useState(false);
+    const [noParentOrSubtypeAlert, setNoParentOrSubtypeAlert] = React.useState(false);
     const [deleteAccountAlert, setDeleteAccountAlert] = React.useState(false);
     const [cannotDeleteAccountAlert, setCannotDeleteAccountAlert] = React.useState(false);
     const toggleDeleteAccountAlert = () => {
@@ -33,45 +37,88 @@ function AccountDetailsEditor(props) {
 
     React.useEffect(() => {
         async function fetchAccountData() {
-            await axios.get(`${API_BASE_URL}/account/${props.selectedAccountId}`).then(response => {
+            if (!props.createMode) {
+                await axios.get(`${API_BASE_URL}/account/${props.selectedAccountId}`).then(response => {
+                    if (response.data) {
+                        setAccountNameInput(response.data.accountName);
+                        setAccountTypeId(response.data.accountTypeId);
+                        setSelectedParentAccountId(response.data.parentAccountId);
+                        setSelectedAccountSubtypeId(response.data.accountSubtypeId);
+                        setInitialDebitValueInput(response.data.initialDebitAmount);
+                        setInitialCreditValueInput(response.data.initialCreditAmount);
+                    }
+                }).catch(console.log);    
+            } else if (props.accountTypeId) {
+                setAccountTypeId(props.accountTypeId);
+            }
+            await axios.get(`${API_BASE_URL}/organization/${appContext.currentOrganizationId}/account`).then(response => {
                 if (response.data) {
-                    setAccountNameInput(response.data.accountName);
-                    setAccountTypeId(response.data.accountTypeId);
-                    setSelectedAccountGroupId(response.data.accountGroupId);
-                    setInitialDebitValueInput(response.data.initialDebitAmount);
-                    setInitialCreditValueInput(response.data.initialCreditAmount);
+                    let validParentAccountOptions = response.data.filter(account => (account.parentAccountId == null && account.debitTotal == 0 && account.creditTotal == 0) || account.hasChildren).map(account => {
+                        return ({
+                            value: account.accountId,
+                            label: account.accountName,
+                            object: account
+                        });
+                    });
+                    validParentAccountOptions.unshift({value: 0, label: "None", object: {}});
+                    setParentAccountOptions(validParentAccountOptions);
                 }
             }).catch(console.log);
-            await axios.get(`${API_BASE_URL}/organization/${appContext.currentOrganizationId}/accountGroup`).then(response => {
+            await axios.get(`${API_BASE_URL}/accountSubtype`).then(response => {
                 if (response.data) {
-                    setAccountGroupOptions(response.data.map(accountGroup => {
+                    setAccountSubtypeOptions(response.data.map(accountSubtype => {
                         return ({
-                            value: accountGroup.accountGroupId,
-                            label: accountGroup.accountGroupName,
-                            object: accountGroup
-                        });
-                    }));
+                            value: accountSubtype.id,
+                            label: accountDetailsEditorText[appContext.locale][accountSubtype.name],
+                            object: accountSubtype
+                        })
+                    }))
                 }
-            })
+            }).catch(console.log);
         }
         fetchAccountData();
-    }, [props.selectedAccountId, props.isOpen])
+    }, [props.selectedAccountId, props.isOpen, props.createMode])
 
     const modalOnClose = () => {
         setNoAccountNameAlert(false);
+        setNoParentOrSubtypeAlert(false);
     }
     const handleSaveButton = () => {
-        let requestBody = {
-            accountId: props.selectedAccountId,
-            accountName: accountNameInput,
-            accountGroupId: selectedAccountGroupId,
-            initialDebitAmount: Number(initialDebitValueInput),
-            initialCreditAmount: Number(initialCreditValueInput)
-        }
-        if (!accountNameInput) {
-            setNoAccountNameAlert(true);
+        
+        let requestBody;
+        if (!selectedParentAccountId) {
+            requestBody = {
+                accountId: props.selectedAccountId,
+                accountName: accountNameInput,
+                accountSubtypeId: selectedAccountSubtypeId,
+                organizationId: appContext.currentOrganizationId,
+                initialDebitAmount: Number(initialDebitValueInput),
+                initialCreditAmount: Number(initialCreditValueInput)
+            }  
         } else {
-            putAccountToServer(requestBody);
+            requestBody = {
+                accountId: props.selectedAccountId,
+                accountName: accountNameInput,
+                parentAccountId: selectedParentAccountId,
+                organizationId: appContext.currentOrganizationId,
+                initialDebitAmount: Number(initialDebitValueInput),
+                initialCreditAmount: Number(initialCreditValueInput)
+            }
+        }
+        if (!accountNameInput || (!selectedParentAccountId && !selectedAccountSubtypeId)) {
+            if (!accountNameInput) {
+                setNoAccountNameAlert(true);
+            }
+            if (!selectedParentAccountId && !selectedAccountSubtypeId) {
+                setNoParentOrSubtypeAlert(true);
+            }
+        } else {
+            if (props.createMode) {
+                requestBody.accountId = null;
+                postAccountToServer(requestBody);
+            } else {
+                putAccountToServer(requestBody);
+            }
         }
     }
     const handleDeleteButton = () => {
@@ -94,8 +141,23 @@ function AccountDetailsEditor(props) {
             toggleCannotDeleteAccountAlert();
         });
     }
-    const handleChangeAccountGroupOption = selectedOption => {
-        setSelectedAccountGroupId(selectedOption.object.accountGroupId);
+    const handleChangeParentAccountOption = selectedOption => {
+        setSelectedParentAccountId(selectedOption.object.accountId);
+        setSelectedAccountSubtypeId(selectedOption.object.accountSubtypeId);
+    }
+
+    const handleChangeAccountSubtypeOption = selectedOption => {
+        setSelectedAccountSubtypeId(selectedOption.object.id);
+        setSelectedParentAccountId(null);
+    }
+
+
+    const postAccountToServer = (requestBody) => {
+        axios.post(`${API_BASE_URL}/account`, requestBody).then(response => {
+            console.log(response);
+            props.fetchData();
+            props.toggle();
+        }).catch(console.log);
     }
 
     const putAccountToServer = (requestBody) => {
@@ -112,7 +174,7 @@ function AccountDetailsEditor(props) {
         <>
             <Modal isOpen={props.isOpen} toggle={props.toggle} onClosed={modalOnClose} centered={true} >
                 <ModalHeader className="bg-light">
-                    {accountDetailsEditorText[appContext.locale]["Edit Account Details"]}
+                    {props.createMode? accountDetailsEditorText[appContext.locale]["Create a New Account"] : accountDetailsEditorText[appContext.locale]["Edit Account Details"]}
                 </ModalHeader>
                 <ModalBody>
                     {noAccountNameAlert ?
@@ -120,7 +182,11 @@ function AccountDetailsEditor(props) {
                             {accountDetailsEditorText[appContext.locale]["Please provide a name for your account."]}
                         </Alert>
                         : null}
-
+                    {noParentOrSubtypeAlert ?
+                        <Alert color="danger">
+                            {accountDetailsEditorText[appContext.locale]["Account must belong to either a subtype or a parent account."]}
+                        </Alert>
+                        : null}
                     <form onSubmit={event => { event.preventDefault(); handleSaveButton() }}>
                         <div className="form-group row">
                             <label className="col-form-label col-md-4">
@@ -139,17 +205,32 @@ function AccountDetailsEditor(props) {
                     </form>
                     <div className="form-group row">
                         <label className="col-form-label col-md-4">
-                            {accountDetailsEditorText[appContext.locale]["Account Group"]}
+                            {accountDetailsEditorText[appContext.locale]["Parent Account"]}
                         </label>
                         <div className="col-md-8">
                             <Select
-                                options={accountGroupOptions.filter(accountGroupOption => accountGroupOption.object.accountTypeId == accountTypeId)}
-                                value={accountGroupOptions.find(accountGroup => accountGroup.object.accountGroupId == selectedAccountGroupId)}
+                                options={parentAccountOptions.filter(option => option.object.accountTypeId == accountTypeId)}
+                                value={parentAccountOptions.find(option => option.object.accountId == selectedParentAccountId)}
                                 isSearchable={true}
-                                onChange={handleChangeAccountGroupOption}
+                                onChange={handleChangeParentAccountOption}
                             />
                         </div>
                     </div>
+                    {appContext.isEnterprise?
+                        <div className="form-group row">
+                        <label className="col-form-label col-md-4">
+                            {accountDetailsEditorText[appContext.locale]["Account Subtype"]}
+                        </label>
+                        <div className="col-md-8">
+                            <Select
+                                options={accountSubtypeOptions.filter(option => option.object.accountType.id == accountTypeId)}
+                                value={accountSubtypeOptions.find(option => option.object.id == selectedAccountSubtypeId)}
+                                isSearchable={true}
+                                onChange={handleChangeAccountSubtypeOption}
+                            />
+                        </div>
+                    </div>
+                    : null}
                     {appContext.isEnterprise? 
                     <form onSubmit={event => { event.preventDefault(); handleSaveButton() }}>
                         <div className="form-group row">
@@ -216,12 +297,16 @@ function AccountDetailsEditor(props) {
                     onConfirm={toggleCannotDeleteAccountAlert}
                     onCancel={toggleCannotDeleteAccountAlert}
                 >
-                    {accountDetailsEditorText[appContext.locale]["Please remove all line items from this account and try again."]}
+                    {accountDetailsEditorText[appContext.locale]["Please remove all line items and child accounts from this account and try again."]}
                 </SweetAlert>
                 : null}
 
         </>
     )
 }
+
+AccountDetailsEditor.defaultProps = {
+    createMode: false
+};
 
 export default AccountDetailsEditor;
