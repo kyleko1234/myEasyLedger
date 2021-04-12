a7f1e3c2ac702e41c3327d8fd8ab167e57cce43c
Kyle Ko <kyleko1234@gmail.com>
2021-04-11 23:06:39 -0700

    changed language settings into a modal

--- front_end/src/pages/settings/settings.js
+++ front_end/src/pages/settings/settings.js
@@ -6,22 +6,13 @@ import { PageSettings } from '../../config/page-settings';
 import { settingsText } from '../../utils/i18n/settings-text';
 import axios from 'axios';
 import { API_BASE_URL, LOCALE_OPTIONS } from '../../utils/constants'
+import LanguageSettingsModal from './language-settings-modal';
 function Settings() {
     const appContext = React.useContext(PageSettings);
-    const [selectedLocale, setSelectedLocale] = React.useState(appContext.locale);
 
-    const handleChangeLocaleOption = (selectedOption) => {
-        setSelectedLocale(selectedOption.value);
-    }
-
-    const handleSaveButton = async () => {
-        let requestBody = {
-            locale: selectedLocale
-        }
-        await axios.patch(`${API_BASE_URL}/person/${appContext.personId}`, requestBody).then(response => {
-            console.log(response);
-        }).catch(console.log);
-        appContext.fetchUserInfo(appContext.personId);
+    const [languageSettingsModal, setLanguageSettingsModal] = React.useState(false);
+    const toggleLanguageSettingsModal = () => {
+        setLanguageSettingsModal(!languageSettingsModal);
     }
 
     return (
@@ -34,29 +25,18 @@ function Settings() {
                 {settingsText[appContext.locale]["Settings"]}
                 <ToggleMobileSidebarButton className="d-md-none float-right " />
             </h1>
-            <div className="widget widget-rounded m-b-15">
+            <div className="widget widget-rounded widget-list widget-list-rounded m-b-15">
                 <div className="widget-header bg-light border-bottom">
                     <h4 className="widget-header-title">{settingsText[appContext.locale]["Personal Settings"]}</h4>
                 </div>
-                <div className="px-3 py-3">
-                    <div className="form-group row">
-                        <label className="col-md-2 col-form-label">
-                            {settingsText[appContext.locale]["Language"]}
-                        </label>
-                        <div className="col-md-4">
-                            <Select
-                                options={LOCALE_OPTIONS}
-                                onChange={handleChangeLocaleOption}
-                                value={LOCALE_OPTIONS.find(localeOption => localeOption.value == selectedLocale)}
-                                menuPortalTarget={document.body}
-                                styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
-                                menuPlacement={'auto'}
-                            />
-                        </div>
+                <Link replace to="#" className="widget-list-item bg-white" onClick={toggleLanguageSettingsModal}>
+                    <div className="widget-list-content px-3">
+                        <div className="widget-list-title">{settingsText[appContext.locale]["Language Settings"]}</div>
                     </div>
-                    <button className="btn btn-primary width-125" onClick={handleSaveButton}>{settingsText[appContext.locale]["Save"]}</button>
-
-                </div>
+                    <div className="m-r-10 widget-list-action text-right">
+                        <i className="fa fa-angle-right fa-lg text-muted"></i>
+                    </div>
+                </Link>
             </div>
             {appContext.permissions.filter(permission => permission.permissionType.id >= 3).length == 0 ? null : //only show this widget if user administrates at least one organization
                 <div className="widget widget-rounded widget-list widget-list-rounded m-b-30">
@@ -77,6 +57,7 @@ function Settings() {
                     })}
                 </div>
             }
+            <LanguageSettingsModal isOpen={languageSettingsModal} toggle={toggleLanguageSettingsModal}/>
         </div>
 
     )
