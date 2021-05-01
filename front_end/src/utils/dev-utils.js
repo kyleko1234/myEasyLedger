3d6f5ecc68e02723a7450d7212ce23796bff0c88
kyleko1234 <kyleko1234@gmail.com>
2021-04-30 17:30:02 -0700

    updated ER diagram to include a journal_entry_log table. Created a file for dev utils on the front end to help with populating sample data.

---
+++ front_end/src/utils/dev-utils.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+/**
+ * Utility script for populating empty db with sample data. This can be more convenient than backing up sample data
+ * from the db layer, since it takes advantage of PostgreSQL triggers and java logging utilities to create realistic data while 
+ * avoiding tedious math or json formatting when changing, adding, or reformatting sample data.
+ */
+function populateSampleUserWithSampleData() {
+    
+}
