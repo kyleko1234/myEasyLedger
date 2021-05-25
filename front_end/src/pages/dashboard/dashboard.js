6fc79b9c7cc3708a9eb16a31f7a271e9aed75ba6
kyleko1234 <kyleko1234@gmail.com>
2021-05-24 23:27:21 -0700

    minor spacing tweak

--- front_end/src/pages/dashboard/dashboard.js
+++ front_end/src/pages/dashboard/dashboard.js
@@ -11,18 +11,18 @@ function Dashboard() {
     return (
         <div>
             {/*<button type="button" onClick={populateSampleUserWithSampleData}>POPULATE SAMPLE DATA</button> */}
-            <div className="row mb-3">
-                <div className="col-xl-8 col-lg-6">
+            <div className="row mb-md-3">
+                <div className="col-xl-8 col-lg-6 mb-3 mb-lg-0">
                     {appContext.isLoading? <div className="d-flex justify-content-center fa-3x py-3"><i className="fas fa-circle-notch fa-spin"></i></div> :
                     <IncomeAndExpenseSummary />}
                 </div>
-                <div className="col-xl-4 col-lg-6">
+                <div className="col-xl-4 col-lg-6 mb-3 mb-lg-0">
                     {appContext.isLoading? <div className="d-flex justify-content-center fa-3x py-3"><i className="fas fa-circle-notch fa-spin"></i></div> : 
                     <BalanceSummary/> }
                 </div>
             </div>
             <div className="row">
-                <div className="col-xl-6 col-lg-6">
+                <div className="col-xl-6 col-lg-6 mb-3 mb-lg-0">
                     {appContext.isLoading? <div className="d-flex justify-content-center fa-3x py-3"><i className="fas fa-circle-notch fa-spin"></i></div> : 
                     <ExpenseBreakdown/>}
                 </div>
