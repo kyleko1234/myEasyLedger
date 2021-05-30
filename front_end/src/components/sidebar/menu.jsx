91faafca3550da8e15d18438b5fa7bceafb2bc09
kyleko1234 <kyleko1234@gmail.com>
2021-05-30 01:17:10 -0700

    icon changes for sidebar

--- front_end/src/components/sidebar/menu.jsx
+++ front_end/src/components/sidebar/menu.jsx
@@ -1,13 +1,13 @@
 export const enterpriseMenu = [
   { path: '/dashboard', icon: 'fas fa-grip-horizontal', title: 'Dashboard', relevantBasePaths: ["dashboard"]},
-  { path: '/journals', icon: 'far fa-list-alt', title: 'Journals', relevantBasePaths: ["journals"]},
-  { path: '/chart-of-accounts', icon: 'fas fa-columns', title: 'Chart of Accounts', relevantBasePaths: ["chart-of-accounts", "account-details"]},
+  { path: '/journals', icon: 'fas fa-book', title: 'Journals', relevantBasePaths: ["journals"]},
+  { path: '/chart-of-accounts', icon: 'far fa-list-alt', title: 'Chart of Accounts', relevantBasePaths: ["chart-of-accounts", "account-details"]},
   { path: '/reports', icon: 'fas fa-envelope-open-text', title: 'Reports', relevantBasePaths: ["reports"]},
 ]
 
 export const personalMenu = [
   { path: '/dashboard', icon: 'fas fa-grip-horizontal', title: 'Dashboard', relevantBasePaths: ["dashboard"]},
-  { path: '/accounts', icon: 'fas fa-columns', title: 'Accounts', relevantBasePaths: ["accounts", "account-details"]},
+  { path: '/accounts', icon: 'far fa-list-alt', title: 'Accounts', relevantBasePaths: ["accounts", "account-details"]},
   { path: '/categories', icon: 'fas fa-columns', title: 'Categories', relevantBasePaths: ["categories", "category-details"]},
   { path: '/reports', icon: 'fas fa-envelope-open-text', title: 'Reports', relevantBasePaths: ["reports"]}
 ]
