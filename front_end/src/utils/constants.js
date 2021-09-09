aee13cdf1356dc2360fe66c72dde9cbdb433041d
kyleko1234 <kyleko1234@gmail.com>
2021-09-09 15:03:26 -0700

    changed api url to production server

--- front_end/src/utils/constants.js
+++ front_end/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080/v0.6';
+export const API_BASE_URL = 'http://143.198.55.173:8080/v0.6';
 export const ACCESS_TOKEN = 'easyLedgerAccessToken';
 export const REFRESH_TOKEN = 'easyLedgerRefreshToken';
 
