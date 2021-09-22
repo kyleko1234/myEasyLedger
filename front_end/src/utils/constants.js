5014750c3f7326cde356cf8890ca828b79432c7e
kyleko1234 <kyleko1234@gmail.com>
2021-09-22 00:50:25 -0700

    changed api url to tls-secured production server api url

--- front_end/src/utils/constants.js
+++ front_end/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://143.198.55.173:8080/v0.6';
+export const API_BASE_URL = 'https://myeasyledger.com:8443/v0.6';
 export const ACCESS_TOKEN = 'easyLedgerAccessToken';
 export const REFRESH_TOKEN = 'easyLedgerRefreshToken';
 
