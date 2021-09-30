2826e35c5397f3ba863ea9c4ddc9e4c3f3d89eda
kyleko1234 <kyleko1234@gmail.com>
2021-09-29 22:55:03 -0700

    added email templates to sendVerificationMail method

--- rest_api/src/main/java/com/easyledger/api/service/EmailDispatchService.java
+++ rest_api/src/main/java/com/easyledger/api/service/EmailDispatchService.java
@@ -25,9 +25,45 @@ public class EmailDispatchService {
 		String message;
 		switch (locale) {
 			case "zh-TW":
+				message = 
+						"<!DOCTYPE html> "
+						+ "<html> "
+						+ "  <head> "
+						+ "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> "
+						+ "  </head> "
+						+ "  <body> "
+						+ "    <p>你好 " + firstName + lastName + "，</p> "
+						+ "    <p>歡迎來到 myEasyLedger。</p> "
+						+ "    <p>請點擊以下連結來起動你的新帳戶：</p> "
+						+ "    <a href=\"https://www.myeasyledger.com/verification/" + token + "\">https://www.myeasyledger.com/verification/" + token + "</a> "
+						+ "    <p>如果點擊上述連結無效，你可以試著拷貝連結貼進你的瀏覽器的視窗中。</p> "
+						+ "    <p> "
+						+ "      <em>本電郵是來自系統的自動回覆，請不要直接回覆本電郵。</em> "
+						+ "    </p> "
+						+ "  </body> "
+						+ "</html>";
+				subject = "myEasyLedger 帳戶確認信";
 				break;
 			case "en-US":
 			default: 
+				message = 
+						"<!DOCTYPE html> "
+						+ "<html> "
+						+ "  <head> "
+						+ "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /> "
+						+ "  </head> "
+						+ "  <body> "
+						+ "    <p>Hello " + firstName + " " + lastName + ",</p> "
+						+ "    <p>Welcome to myEasyLedger.</p> "
+						+ "    <p>Please click the following URL to activate your account:</p> "
+						+ "    <a href=\"https://www.myeasyledger.com/verification/" + token + "\">https://www.myeasyledger.com/verification/" + token + "</a> "
+						+ "    <p>If clicking the URL above does not work, copy and paste the URL into a browser window.</p> "
+						+ "    <p> "
+						+ "      <em>This is an automated email, please do not reply directly to this email.</em> "
+						+ "    </p> "
+						+ "  </body> "
+						+ "</html>";
+				subject = "myEasyLedger Account Verification";
 				break;
 		}
 	}
