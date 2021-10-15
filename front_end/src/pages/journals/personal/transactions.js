4e024b929946b573bae654659f59b24be8990e36
kyleko1234 <kyleko1234@gmail.com>
2021-10-15 00:52:38 -0700

    added transaction table component to transaction page

--- front_end/src/pages/journals/personal/transactions.js
+++ front_end/src/pages/journals/personal/transactions.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import axios from 'axios';
+import { transactionsText } from '../../../utils/i18n/transactions-text';
+import { PageSettings } from '../../../config/page-settings';
+import { API_BASE_URL } from '../../../utils/constants';
+import TableOfTransactions from './table-of-transactions';
 
 function Transactions() {
+    const appContext = React.useContext(PageSettings);
+    const columns = [ // accessor is the "key" in the data},
+                { header: transactionsText[appContext.locale]['Date'], accessor: 'journalEntryDate', className: " col-md-2 text-nowrap " },
+                { header: transactionsText[appContext.locale]['Description'], accessor: 'description', className: " col-md-6 text-truncate " },
+                { header: transactionsText[appContext.locale]['Inflow'], accessor: 'debitAmount', className: " text-right col-md-2 text-nowrap " },
+                { header: transactionsText[appContext.locale]['Outflow'], accessor: 'creditAmount', className: " text-right col-md-2 text-nowrap " },
+    ];
 
+    const [pageSize, setPageSize] = React.useState(10);
+    const [pageIndex, setPageIndex] = React.useState(0);
+    const [data, setData] = React.useState([]);
+    const [totalPages, setTotalPages] = React.useState(0);
+    const [totalElements, setTotalElements] = React.useState(0);
+    const [pageLength, setPageLength] = React.useState(0);
+    const [first, setFirst] = React.useState(true);
+    const [last, setLast] = React.useState(true);
+    const [loading, setLoading] = React.useState(true);
+    const previousPage = () => setPageIndex(pageIndex - 1);
+    const nextPage = () => setPageIndex(pageIndex + 1);
+    
+        
+    const fetchData = React.useCallback(async ( pageIndex, pageSize ) => {
+        // This will get called when the table needs new data
+        //fetch data from Easy Ledger API
+        setLoading(true);
+        async function fetchTableData () {
+            const url = `${API_BASE_URL}/organization/${appContext.currentOrganizationId}/assetAndLiabilityLineItem/?page=${pageIndex}&size=${pageSize}`;
+            await axios.get(url).then(response => {
+                var dataContent = response.data.content;
+                dataContent.forEach(lineItem => {
+                    if (lineItem.isCredit) {
+                        lineItem.debitAmount = 0;
+                        lineItem.creditAmount = lineItem.amount;
+                    } else {
+                        lineItem.creditAmount = 0;
+                        lineItem.debitAmount = lineItem.amount;
+                    }
+                })
+                setData(dataContent);
+                setTotalPages(response.data.totalPages);
+                setTotalElements(response.data.totalElements);
+                setPageLength(response.data.numberOfElements);
+                setFirst(response.data.first);
+                setLast(response.data.last);
+            })
+                .catch(console.log);
+        }
+        await fetchTableData();
+        setLoading(false);
+    }, [])
+
+    return(
+        <>
+            <TableOfTransactions
+                tableTitle={ <div className="h1">Transactions</div>}
+                hasAddEntryButton={true}
+                fetchData={fetchData}
+                pageSize={pageSize}
+                pageIndex={pageIndex}
+                columns={columns}
+                data={data}
+                totalPages={totalPages}
+                totalElements={totalElements}
+                pageLength={pageLength}
+                first={first}
+                last={last}
+                loading={loading}
+                previousPage={previousPage}
+                nextPage={nextPage}
+                category={false}                            
+            /> 
+        </>
+    )
 }
 
 export default Transactions;
---
+++ front_end/src/utils/i18n/transactions-text.js
@@ -0,0 +1,20 @@
+export const transactionsText = {
+    "en-US": {
+        "Date": "Date",
+        "Description": "Description",
+        "Debit": "Debit",
+        "Credit": "Credit",
+        "Inflow": "Inflow",
+        "Outflow": "Outflow",
+        "Account": "Account"
+    },
+    "zh-TW": {
+        "Date": "日期",
+        "Description": "描述",
+        "Debit": "借方",
+        "Credit": "貸方",
+        "Inflow": "流入",
+        "Outflow": "流出",
+        "Account": "TRANSLATION"
+    },
+}
