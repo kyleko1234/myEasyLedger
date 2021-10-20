0fd6b4bc9f53099403cc492477e8d7061680e083
kyleko1234 <kyleko1234@gmail.com>
2021-10-20 16:40:12 -0700

    wrote function to handle currency formatting for display purposes

--- front_end/src/utils/util-fns.js
+++ front_end/src/utils/util-fns.js
@@ -1,6 +1,38 @@
+/**
+ * Takes a string. Returns true if the string is [four digits]-[two digits]-[two digits], 
+ * which implies a date in the format yyyy-mm-dd. Else, returns false.
+ * @param {String} dateString 
+ * @returns {Boolean}
+ */
 export const validateDate = (dateString) => {
     if (!dateString.match(/^\d{4}-\d{2}-\d{2}$/)) {
         return false;
     }
     return true;
+}
+
+/**
+ * Takes a locale, currency, and a number and returns a formatted string representing the number as a currency. 
+ * @param {String} locale 
+ * @param {String} currency 
+ * @param {Number} amount
+ * @returns {String}
+ */
+export const formatCurrency = (locale, currency, amount) => {
+    switch (locale) {
+        case "zh-TW":
+            switch (currency) {
+                case "TWD":
+                    return `$${Math.round(amount)}`
+                default: 
+                    return new Intl.NumberFormat(locale, { style: 'currency', currency: currency }).format(amount);
+            }
+        default: 
+            switch (currency) {
+                case "TWD":
+                    return `NT$${Math.round(amount)}`
+                default:
+                    return new Intl.NumberFormat(locale, { style: 'currency', currency: currency }).format(amount);
+            }
+    }
 }
