c77975da5c8cbe0b363d5782e18fb0f1f2d18e71
kyleko1234 <kyleko1234@gmail.com>
2021-10-24 18:57:22 -0700

    hid edit permission button for persons with lower or equal permissions

--- front_end/src/pages/settings/components/organization-roster.js
+++ front_end/src/pages/settings/components/organization-roster.js
@@ -150,7 +150,7 @@ function OrganizationRoster(props) {
                                                 <div className="td col-4">{person.permissionTypeName}</div>
                                                 <div className="td col-1">
                                                     <Link replace
-                                                        className={"text-muted" + (person.permissionTypeId < appContext.permissions.find(permission => permission.organization.id == props.organizationId).permissionType.id ? "" : " visibility-hidden")}
+                                                        className={"text-muted" + (person.permissionTypeId <= ownPermissionForCurrentOrganization.permissionTypeId ? " " : " invisible")}
                                                         to="#" onClick={() => handleEditAPersonButton(person)}>
                                                         <i className="fa fa-edit"></i>
                                                     </Link>
