ad5911cd0da1f0163c8df786552052e3588a9ffe
kyleko1234 <kyleko1234@gmail.com>
2022-01-20 23:56:41 -0800

    flagged translations

--- front_end/src/components/misc/timeout-sweetalert.js
+++ front_end/src/components/misc/timeout-sweetalert.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import SweetAlert from 'react-bootstrap-sweetalert';
+import { PageSettings } from '../../config/page-settings';
+import timeoutSweetalertText from '../../utils/i18n/timeout-sweetalert-text';
 
 //required props: isOpen, toggle
 function TimeoutSweetAlert(props) {
+    const appContext = React.useContext(PageSettings);
+
     if (props.isOpen) {
         return(
             <SweetAlert 
                 danger
                 showConfirm={false} 
-                showCancel={true}
-                cancelBtnBsStyle="default"
-                cancelBtnText="Okay!"
-                title="Your request timed out."
+                showCancel={false}
+                title={timeoutSweetalertText[appContext.locale]["Your request timed out."]}
                 onCancel={props.toggle}
             >
-                We took too long to process this request. Please try again later.
+                {timeoutSweetalertText[appContext.locale]["We took too long to process this request. Please try again later."]}
             </SweetAlert>
         )
     } else {
---
+++ front_end/src/utils/i18n/timeout-sweetalert-text.js
@@ -0,0 +1,10 @@
+export default timeoutSweetAlertText = {
+    "en-US" : {
+        "Your request timed out.": "Your request timed out.",
+        "We took too long to process this request. Please try again later.": "We took too long to process this request. Please try again later."
+    },
+    "zh-TW": {
+        "Your request timed out.": "TRANSLATION",
+        "We took too long to process this request. Please try again later.": "TRANSLATION"
+    }
+}
