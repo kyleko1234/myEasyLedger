4373610af6604c0fd2028703ecc3887154ac3395
kyleko1234 <kyleko1234@gmail.com>
2022-03-01 22:54:21 -0800

    added date range controls to expense report

--- front_end/src/pages/reports/components/date-range-controls.js
+++ front_end/src/pages/reports/components/date-range-controls.js
@@ -1,7 +1,129 @@
 import React from 'react';
+import { Alert, Card, CardBody } from 'reactstrap';
+import { PageSettings } from '../../../config/page-settings';
+import { incomeStatementRenderText } from '../../../utils/i18n/income-statement-render-text';
+import Select from 'react-select';
+import { Link } from 'react-router-dom';
 
-function DateRangeControls(props) {
 
+function DateRangeControls({datesToRequest, invalidDateAlert, handleUpdateReportButton, handleRemoveDateRangeButton, dateRangePresets, handleSelectDateRangePreset, handleChangeStartDate, handleChangeEndDate, detailedView, toggleDetailedView, handleCompareButton }) {
+    const appContext = React.useContext(PageSettings);
+    
+    return(
+        <Card className="very-rounded shadow-sm bg-light my-4">
+            <CardBody className="">
+                <Alert isOpen={invalidDateAlert} color="danger">
+                    {incomeStatementRenderText[appContext.locale]["Invalid date(s) selected."]}
+                </Alert>
+                <form onSubmit={handleUpdateReportButton}>
+                    <div className="mb-2">
+                        <h2 className="h5 my-0">{incomeStatementRenderText[appContext.locale]["Options"]}</h2>
+                    </div>
+                    <div className="d-none d-md-block">
+                        {datesToRequest.map((dateObject, i) => {
+                            return (
+                                <div key={i}>
+                                    {datesToRequest.length > 1
+                                    ?   <div className="font-weight-semibold my-1 d-flex align-items-center">
+                                            {incomeStatementRenderText[appContext.locale]["Date range"] + " " + (i + 1)}
+                                            <button className="btn btn-light py-0 px-1 mx-1 border-0" onClick={() => handleRemoveDateRangeButton(i)}>
+                                                <i className="ion ion-md-close fa-fw"></i>
+                                            </button>
+                                        </div>
+                                    : null}
+                                    <div className="d-flex w-100 align-items-center mb-2">
+                                        <Select
+                                            className="col-4 px-0"
+                                            classNamePrefix="form-control"
+                                            options={dateRangePresets}
+                                            menuPortalTarget={document.body}
+                                            menuShouldScrollIntoView={false}
+                                            styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+                                            onChange={selectedOption => handleSelectDateRangePreset(selectedOption, i)}
+                                            placeholder={"Custom"}
+                                            value={datesToRequest[i].label === "Custom" ? null : dateRangePresets.find(preset => preset.label == datesToRequest[i].label)}
+                                        />
+                                        <label className="my-0 text-right col-1 px-2">{incomeStatementRenderText[appContext.locale]["From:"]} </label>
+                                        <input type="date" placeholder={incomeStatementRenderText[appContext.locale]["yyyy-mm-dd"]} className="form-control col-3" value={datesToRequest[i].startDate} onChange={event => handleChangeStartDate(event.target.value, i)} />
+                                        <label className="my-0 text-right col-1 px-2">{incomeStatementRenderText[appContext.locale]["To:"]} </label>
+                                        <input type="date" placeholder={incomeStatementRenderText[appContext.locale]["yyyy-mm-dd"]} className="form-control col-3" value={datesToRequest[i].endDate} onChange={event => handleChangeEndDate(event.target.value, i)} />
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="d-md-none">
+                        {datesToRequest.map((dateObject, i) => {
+                            return (
+                                <div key={i}>
+                                    <div className="d-flex my-1 justify-content-between">
+                                        <div className="font-weight-semibold d-flex align-items-center">
+                                            {incomeStatementRenderText[appContext.locale]["Date range"] + (datesToRequest.length > 1? (" " + (i + 1)) : "" )}
+                                            <button className={"btn btn-light py-0 px-1 mx-1 border-0 " + (datesToRequest.length > 1 ? "" : " invisible")} onClick={() => handleRemoveDateRangeButton(i)}>
+                                                <i className="ion ion-md-close fa-fw"></i>
+                                            </button>
+                                        </div>
+                                        <Select
+                                            className="col-6 px-0"
+                                            classNamePrefix="form-control"
+                                            options={dateRangePresets}
+                                            menuPortalTarget={document.body}
+                                            menuShouldScrollIntoView={false}
+                                            styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+                                            onChange={selectedOption => handleSelectDateRangePreset(selectedOption, i)}
+                                            placeholder={"Custom"}
+                                            value={datesToRequest[i].label === "Custom" ? null : dateRangePresets.find(preset => preset.label == datesToRequest[i].label)}
+                                        />
+                                    </div>
+                                    <div className="d-flex justify-content-between text-left align-items-center my-1">
+                                        <label className="my-0 col-3 px-0">
+                                            {incomeStatementRenderText[appContext.locale]["From:"]} 
+                                        </label>
+                                        <input 
+                                            type="date" 
+                                            placeholder={incomeStatementRenderText[appContext.locale]["yyyy-mm-dd"]}
+                                            className="form-control" 
+                                            value={datesToRequest[i].startDate} 
+                                            onChange={event => handleChangeStartDate(event.target.value, i)} 
+                                        />
+                                    </div>
+                                    <div className="d-flex justify-content-between text-left align-items-center mb-2">
+                                        <label className="my-0 col-3 px-0">
+                                            {incomeStatementRenderText[appContext.locale]["To:"]} 
+                                        </label>
+                                        <input 
+                                            type="date" 
+                                            placeholder={incomeStatementRenderText[appContext.locale]["yyyy-mm-dd"]}
+                                            className="form-control" 
+                                            value={datesToRequest[i].endDate} 
+                                            onChange={event => handleChangeEndDate(event.target.value, i)} 
+                                        />
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    {datesToRequest.length < 3
+                        ?   <div className="mb-2">
+                                <Link replace to="#" onClick={handleCompareButton} className="text-decoration-none">
+                                    <i className="ion ion-md-add"></i> {incomeStatementRenderText[appContext.locale]["Compare"]}
+                                </Link>
+                            </div>
+                        : null
+                    }
+                    <div className="d-flex align-items-center justify-content-between mt-2">
+                        <div className="custom-control custom-switch">
+                            <input type="checkbox" id="detailedViewCheckbox" className="custom-control-input" value={detailedView} onChange={toggleDetailedView} />
+                            <label htmlFor="detailedViewCheckbox" className="my-0 custom-control-label">{incomeStatementRenderText[appContext.locale]["Detailed View"]}</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary width-200" onClick={handleUpdateReportButton}>
+                            {incomeStatementRenderText[appContext.locale]["Update report"]}
+                        </button>
+                    </div>
+                </form>
+            </CardBody>
+        </Card>
+    )
 }
 
 export default DateRangeControls;
--- front_end/src/pages/reports/expense-report.js
+++ front_end/src/pages/reports/expense-report.js
@@ -1,25 +1,180 @@
 import React from 'react';
 import { PageSettings } from '../../config/page-settings';
-import IncomeStatementRender from './components/income-statement-render';
-import {incomeStatementReportText} from '../../utils/i18n/income-statement-report-text';
 import { useParams } from 'react-router';
 import LoadingSpinner from '../../components/misc/loading-spinner';
+import DateRangeControls from './components/date-range-controls';
+import { formatCurrency, getDateInCurrentYear, getTodayAsDateString, validateDate } from '../../utils/util-fns';
+import axios from 'axios';
+import { API_BASE_URL } from '../../utils/constants';
 
 function ExpenseReport() {
     const appContext = React.useContext(PageSettings);
     const params = useParams();
+    const defaultStartDate = params.startDate;
+    const defaultEndDate = params.endDate;
+    const today = getTodayAsDateString();
+    const [invalidDateAlert, setInvalidDateAlert] = React.useState(false);
+
+    const beginningOfCurrentFiscalYear = getDateInCurrentYear(appContext.permissions.find(permission => permission.organization.id === appContext.currentOrganizationId).organization.fiscalYearBegin);
+    const [detailedView, setDetailedView] = React.useState(false);
+    const toggleDetailedView = () => setDetailedView(!detailedView);
+    const [loading, setLoading] = React.useState(true);
+
+    const [incomeStatementObjects, setIncomeStatementObjects] = React.useState([]);
+    const [datesToRequest, setDatesToRequest] = React.useState([{
+        label: "Custom",
+        startDate: defaultStartDate ? defaultStartDate : beginningOfCurrentFiscalYear, //cannot use defaultProps here because we would need to call appContext at the top level, so conditional is used here instead
+        endDate: defaultEndDate ? defaultEndDate : today
+    }]);
+    const [columnLabels, setColumnLabels] = React.useState([]);
+    const [dateRangePresets, setDateRangePresets] = React.useState([]);
+
+    const requestIncomeStatementObjects = async arrayToStoreObjects => {
+        let newColumnLabels = [];
+        for (const dateObject of datesToRequest) {
+            await axios.get(`${API_BASE_URL}/organization/${appContext.currentOrganizationId}/reports/incomeStatement/${dateObject.startDate}/${dateObject.endDate}`).then(response => {
+                newColumnLabels.push(dateObject);     //column labels are not updated until the report is actually updated
+                arrayToStoreObjects.push(response.data);
+            }).catch(console.log);
+        }
+        setColumnLabels(newColumnLabels);
+    }
+
+    React.useEffect(() => {
+        async function fetchData() {
+            setLoading(true);
+            await axios.get(`${API_BASE_URL}/organization/${appContext.currentOrganizationId}/dateRangePresetsUpToDate/${today}/${appContext.locale}`).then(response => {
+                setDateRangePresets(response.data);
+            })
+            let fetchedIncomeStatementObjects = [];
+            await requestIncomeStatementObjects(fetchedIncomeStatementObjects);
+            setIncomeStatementObjects(fetchedIncomeStatementObjects);
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    const handleChangeStartDate = (date, i) => {
+        let newDatesToRequestArray = datesToRequest.slice();
+        newDatesToRequestArray[i] = {
+            label: "Custom", 
+            startDate: date, 
+            endDate: newDatesToRequestArray[i].endDate
+        }
+        setDatesToRequest(newDatesToRequestArray)
+    }
+    const handleChangeEndDate = (date, i) => {
+        let newDatesToRequestArray = datesToRequest.slice();
+        newDatesToRequestArray[i] = {
+            label: "Custom", 
+            startDate: newDatesToRequestArray[i].startDate, 
+            endDate: date
+        }
+        setDatesToRequest(newDatesToRequestArray)
+    }
+    const numberAsCurrency = (number) => {
+        if (number == 0) {
+            return formatCurrency(appContext.locale, appContext.currency, 0);
+        }
+        return formatCurrency(appContext.locale, appContext.currency, number);
+    }
+
+    const handleSelectDateRangePreset = (selectedOption, i) => {
+        if (selectedOption) {
+            let dateToRequestObject = {
+                label: selectedOption.label,
+                startDate: selectedOption.object.startDate,
+                endDate: selectedOption.object.endDate
+            };
+            let newDatesToRequestArray = datesToRequest.slice();
+            newDatesToRequestArray[i] = dateToRequestObject;
+            setDatesToRequest(newDatesToRequestArray);
+        }
+    }
+
+    const handleRemoveDateRangeButton = i => {
+        let datesArray = datesToRequest.slice();
+        datesArray.splice(i, 1);
+        setDatesToRequest(datesArray);
+    }
+
+    const handleCompareButton = () => {
+        let datesArray = datesToRequest.slice();
+        datesArray.push({
+            label: "Custom", 
+            startDate: beginningOfCurrentFiscalYear,
+            endDate:today}
+        )
+        setDatesToRequest(datesArray);
+    }
+    
+    const validateDatesToRequest = datesToRequest => {
+        let returnedBoolean = true
+        datesToRequest.forEach(dateToRequestObject => {
+            if (!(validateDate(dateToRequestObject.startDate) && validateDate(dateToRequestObject.endDate))) {
+                returnedBoolean = false;
+            }
+        })
+        return returnedBoolean;
+    }
+
+    const handleUpdateReportButton = async (event) => {
+        event.preventDefault();
+        setInvalidDateAlert(false);
+        setLoading(true);
+        if (validateDatesToRequest(datesToRequest)) {
+            let fetchedIncomeStatementObjects = [];
+            await requestIncomeStatementObjects(fetchedIncomeStatementObjects);
+            setIncomeStatementObjects(fetchedIncomeStatementObjects);    
+        } else {
+            setInvalidDateAlert(true);
+        }
+        setLoading(false);
+    }
+
+    const sumDebitsAndCreditsOfChildren = (accountId, indexOfIncomeStatementObject) => {
+        let total = 0;
+        let childAccounts = incomeStatementObjects[indexOfIncomeStatementObject]
+            .accountBalances
+            .filter(childAccount => childAccount.parentAccountId === accountId);
+        childAccounts
+            .forEach(account => {
+                if (account.debitsMinusCredits) {
+                    total += account.debitsMinusCredits;
+                }
+        })
+        return total;
+    }
 
     return (
         <div>
             <h1 className="page-header">
                 Expense Report
             </h1>
+            <DateRangeControls 
+                datesToRequest={datesToRequest}
+                invalidDateAlert={invalidDateAlert}
+                handleUpdateReportButton={handleUpdateReportButton}
+                handleRemoveDateRangeButton={handleRemoveDateRangeButton}
+                dateRangePresets={dateRangePresets}
+                handleSelectDateRangePreset={handleSelectDateRangePreset}
+                handleChangeStartDate={handleChangeStartDate}
+                handleChangeEndDate={handleChangeEndDate}
+                detailedView={detailedView}
+                toggleDetailedView={toggleDetailedView}
+                handleCompareButton={handleCompareButton}
+            />
             <div>
                 {appContext.isLoading
                     ? <LoadingSpinner big />
-                    : <div>
-                        REPORT GOES HERE
-                    </div>
+                    : (appContext.isEnterprise
+                        ? <div>
+                            ENTERPRISE REPORT
+                        </div>
+                        : <div>
+                            PERSONAL REPORT
+                        </div>
+                    )
                 }
             </div>
 		</div>
