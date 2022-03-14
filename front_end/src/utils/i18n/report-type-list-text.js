c63893b831ab66c8ff9c90c065435152b5476d9a
Hsufeng Ko <hsufeng@gmail.com>
2022-03-14 01:26:30 -0700

    Translate Expense Report

--- front_end/src/utils/i18n/report-type-list-text.js
+++ front_end/src/utils/i18n/report-type-list-text.js
@@ -15,7 +15,7 @@ export const reportTypeListText = {
         "Net Worth": "淨資產報告",
         "Income and Expense Report": "收入支出報告",
         "Cash Flow": "現金流量表",
-        "Expense Report": "TRANSLATION"
+        "Expense Report": "支出報告"
 
     }
 }
