0b5ad958ab49e685872adfdaee25a1b2e5b8f031
kyleko1234 <kyleko1234@gmail.com>
2022-05-30 16:08:10 -0700

    added createvendorfromdto method

--- rest_api/src/main/java/com/easyledger/api/service/VendorService.java
+++ rest_api/src/main/java/com/easyledger/api/service/VendorService.java
@@ -3,6 +3,10 @@ package com.easyledger.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.easyledger.api.dto.VendorDTO;
+import com.easyledger.api.exception.ResourceNotFoundException;
+import com.easyledger.api.model.Organization;
+import com.easyledger.api.model.Vendor;
 import com.easyledger.api.repository.OrganizationRepository;
 import com.easyledger.api.repository.VendorRepository;
 
@@ -21,5 +25,20 @@ public class VendorService {
 		this.organizationRepo = organizationRepo;
 	}
 	
-	
+	public Vendor createVendorFromDTO(VendorDTO dto) throws ResourceNotFoundException {
+		Vendor product = new Vendor();
+		Organization organization = organizationRepo.findById(dto.getOrganizationId())
+	    		.orElseThrow(() -> new ResourceNotFoundException("Organization not found for this id :: " + dto.getOrganizationId())); 
+		product.setOrganization(organization);
+		if (dto.getVendorName() != null) {
+			product.setVendorName(dto.getVendorName());
+		}
+		if (dto.getContactName() != null) {
+			product.setContactName(dto.getContactName());
+		}
+		if (dto.getEmail() != null) {
+			product.setEmail(dto.getEmail());
+		}
+		return product;
+	}
 }
