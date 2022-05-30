66234eba385334820162a025de8cea4bed7a7cb6
kyleko1234 <kyleko1234@gmail.com>
2022-05-30 16:42:19 -0700

    added createvendor method

--- rest_api/src/main/java/com/easyledger/api/controller/VendorController.java
+++ rest_api/src/main/java/com/easyledger/api/controller/VendorController.java
@@ -1,14 +1,19 @@
 package com.easyledger.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.easyledger.api.dto.VendorDTO;
+import com.easyledger.api.exception.ConflictException;
 import com.easyledger.api.exception.ResourceNotFoundException;
 import com.easyledger.api.exception.UnauthorizedException;
 import com.easyledger.api.model.Vendor;
@@ -44,4 +49,16 @@ public class VendorController {
         return ResponseEntity.ok().body(dto);
     }	
 
+    @PostMapping("/vendor")
+    @ResponseStatus(HttpStatus.CREATED)
+    public VendorDTO createVendor(@RequestBody VendorDTO dto, Authentication authentication) 
+    		throws ConflictException, UnauthorizedException, ResourceNotFoundException {
+    	if (dto.getVendorId() != null) {
+    		throw new ConflictException("Please do not attempt to manually create a vendorId.");
+    	}
+    	authorizationService.authorizeEditPermissionsByOrganizationId(authentication, dto.getOrganizationId());
+    	Vendor updatedVendor = vendorService.createVendorFromDTO(dto);
+    	vendorRepo.save(updatedVendor);
+    	return new VendorDTO(updatedVendor);
+    }
 }
