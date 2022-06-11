791c6bf2ed6a7d77c42104ac5f99e8fae50e9924
kyleko1234 <kyleko1234@gmail.com>
2022-06-11 01:10:13 -0700

    fixed double post/put error

--- front_end/src/pages/chart-of-accounts/components/account-details-editor.js
+++ front_end/src/pages/chart-of-accounts/components/account-details-editor.js
@@ -400,7 +400,7 @@ function AccountDetailsEditor(props) {
                         </div>
                         <div>
                             <div id="save-button" className="d-inline-block">
-                                <button type="submit" className="btn btn-primary width-10ch" onClick={handleSaveButton} disabled={appContext.currentPermissionTypeId < 2 ? true : false}>
+                                <button type="submit" className="btn btn-primary width-10ch" disabled={appContext.currentPermissionTypeId < 2 ? true : false}>
                                     {accountDetailsEditorText[appContext.locale]["Save"]}
                                 </button>
                             </div>
