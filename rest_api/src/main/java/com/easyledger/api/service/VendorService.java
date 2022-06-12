ff728a01314d0fe675233592bf47179f41ff3139
kyleko1234 <kyleko1234@gmail.com>
2022-06-11 20:08:29 -0700

    put/post vendor will now reject duplicate names with a 409

--- rest_api/src/main/java/com/easyledger/api/service/VendorService.java
+++ rest_api/src/main/java/com/easyledger/api/service/VendorService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.easyledger.api.dto.VendorDTO;
+import com.easyledger.api.exception.ConflictException;
 import com.easyledger.api.exception.ResourceNotFoundException;
 import com.easyledger.api.exception.UnauthorizedException;
 import com.easyledger.api.model.Organization;
@@ -12,6 +13,7 @@ import com.easyledger.api.model.Vendor;
 import com.easyledger.api.repository.OrganizationRepository;
 import com.easyledger.api.repository.VendorRepository;
 import com.easyledger.api.security.AuthorizationService;
+import com.easyledger.api.utility.Utility;
 
 @Service
 public class VendorService {
@@ -32,10 +34,13 @@ public class VendorService {
 		this.authorizationService = authorizationService;
 	}
 	
-	public Vendor createVendorFromDTO(VendorDTO dto) throws ResourceNotFoundException {
+	public Vendor createVendorFromDTO(VendorDTO dto) throws ResourceNotFoundException, ConflictException {
 		Vendor product = new Vendor();
 		Organization organization = organizationRepo.findById(dto.getOrganizationId())
 	    		.orElseThrow(() -> new ResourceNotFoundException("Organization not found for this id :: " + dto.getOrganizationId())); 
+		if (isDuplicateVendorName(organization.getId(), dto.getVendorName())) {
+			throw new ConflictException("Duplicate vendor name.");
+		}
 		product.setOrganization(organization);
 		if (dto.getVendorName() != null) {
 			product.setVendorName(dto.getVendorName());
@@ -50,8 +55,11 @@ public class VendorService {
 	}
 	
 	public Vendor updateVendorFromDTO(VendorDTO dto, Authentication authentication) 
-			throws ResourceNotFoundException, UnauthorizedException {
+			throws ResourceNotFoundException, UnauthorizedException, ConflictException {
 		authorizationService.authorizeEditPermissionsByOrganizationId(authentication, dto.getOrganizationId());
+		if (isDuplicateVendorName(dto.getOrganizationId(), dto.getVendorName())) {
+			throw new ConflictException("Duplicate vendor name.");
+		}
 		Vendor vendor = vendorRepo.findById(dto.getVendorId())
 	    		.orElseThrow(() -> new ResourceNotFoundException("Vendor not found for this id :: " + dto.getVendorId())); 
 		authorizationService.authorizeEditPermissionsByOrganizationId(authentication, vendor.getOrganization().getId());
@@ -68,5 +76,9 @@ public class VendorService {
 			vendor.setEmail(dto.getEmail());
 		}
 		return vendorRepo.save(vendor);
+	}
+	
+	private boolean isDuplicateVendorName(Long organizationId, String name) {
+		return vendorRepo.isDuplicateVendorName(organizationId, Utility.trimString(name.toLowerCase(), 64));
 	}
 }
