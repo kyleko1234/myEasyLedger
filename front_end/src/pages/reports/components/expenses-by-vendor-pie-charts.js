8ae978db2b313dff90a49c4d7453ffb02240ba43
kyleko1234 <kyleko1234@gmail.com>
2022-10-12 07:05:54 +0000

    pie chart colors are now in one place

--- front_end/src/pages/reports/components/expenses-by-vendor-pie-charts.js
+++ front_end/src/pages/reports/components/expenses-by-vendor-pie-charts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 import LoadingSpinner from '../../../components/misc/loading-spinner';
 import { PageSettings } from '../../../config/page-settings';
+import { PIE_CHART_BACKGROUND_COLORS, PIE_CHART_BORDER_COLORS } from '../../../utils/constants';
 import { incomeStatementRenderText } from '../../../utils/i18n/income-statement-render-text';
 import { formatCurrency, getPercentage, localizeDate } from '../../../utils/util-fns';
 
@@ -28,15 +29,9 @@ function ExpensesByVendorPieCharts({columnLabels, expensesByVendorReports, loadi
                     labels: [],
                     datasets: [{
                         data: [],
-                        borderColor: [
-                            "rgba(37, 137, 189, 1)", "rgba(176, 124, 158, 1)", "rgba(205, 198, 174, 1)", "rgba(56, 104, 106, 1)", "rgba(219, 216, 240, 1)", "rgba(163, 180, 162, 1)", "rgba(181, 145, 148, 1)", "rgba(54, 17, 52, 1)", "rgba(10, 35, 66, 1)", "rgba(80, 61, 66, 1)", 
-                            "rgba(255, 228, 94, 1)", "rgba(32, 191, 85, 1)", "rgba(119, 135, 139, 1)", "rgba(190, 255, 199, 1)", "rgba(55, 62, 64, 1)", "rgba(72, 130, 134, 1)", "rgba(183, 213, 212, 1)", "rgba(255, 133, 82, 1)", "rgba(236, 145, 146, 1)", "rgba(255, 32, 110, 1)"
-                        ],
+                        borderColor: PIE_CHART_BORDER_COLORS,
                         borderWidth: 2,
-                        backgroundColor: [
-                            "rgba(37, 137, 189, 0.7)", "rgba(176, 124, 158, 0.7)", "rgba(205, 198, 174, 0.7)", "rgba(56, 104, 106, 0.7)", "rgba(219, 216, 240, 0.7)", "rgba(163, 180, 162, 0.7)", "rgba(181, 145, 148, 0.7)", "rgba(54, 17, 52, 0.7)", "rgba(10, 35, 66, 0.7)", "rgba(80, 61, 66, 0.7)", 
-                            "rgba(255, 228, 94, 0.7)", "rgba(32, 191, 85, 0.7)", "rgba(119, 135, 139, 0.7)", "rgba(190, 255, 199, 0.7)", "rgba(55, 62, 64, 0.7)", "rgba(72, 130, 134, 0.7)", "rgba(183, 213, 212, 0.7)", "rgba(255, 133, 82, 0.7)", "rgba(236, 145, 146, 0.7)", "rgba(255, 32, 110, 0.7)"
-                        ]        
+                        backgroundColor: PIE_CHART_BACKGROUND_COLORS       
                     }]
                 },
                 options: {
