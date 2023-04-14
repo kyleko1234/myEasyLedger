0ccf550f079afb5b89817b989bf999e297d38a65
kyleko1234 <kyleko1234@gmail.com>
2023-04-13 23:50:51 -0700

    added util function to add lists of bigdecimal

--- rest_api/src/main/java/com/easyledger/api/utility/Utility.java
+++ rest_api/src/main/java/com/easyledger/api/utility/Utility.java
@@ -1,5 +1,9 @@
 package com.easyledger.api.utility;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Utility {
 	
 	private Utility() {	
@@ -12,5 +16,26 @@ public final class Utility {
 		} else {
 			return trimmedString.substring(0, maxLength);
 		}
+	}
+	
+	public static List<BigDecimal> addLists(List<BigDecimal> list1, List<BigDecimal> list2) {
+		ArrayList<BigDecimal> returnedList = new ArrayList<BigDecimal>();
+		List<BigDecimal> largerList;
+		List<BigDecimal> smallerList;
+		if (list1.size() >= list2.size()) {
+			largerList = list1;
+			smallerList = list2;
+		} else {
+			largerList = list2;
+			smallerList = list1;
+		}
+		for (int i = 0; i < largerList.size(); i++) {
+			if (i >= smallerList.size()) {
+				returnedList.add(largerList.get(i));
+			} else {
+				returnedList.add(largerList.get(i).add(smallerList.get(i)));
+			}
+		}
+		return returnedList;
 	}
 }
