dfa4659f380270546f8d0b5d36f1a9eaa69db64c
kyleko1234 <kyleko1234@gmail.com>
2023-09-02 01:23:50 -0700

    updated pie charts. truncated tooltip is not easily solvable, will make it a low priority to-do

---
+++ front_end/src/pages/reports/components/expenses-by-account-pie-charts.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import LoadingSpinner from '../../../components/misc/loading-spinner';
+import { PageSettings } from '../../../config/page-settings';
+import { PIE_CHART_BACKGROUND_COLORS, PIE_CHART_BORDER_COLORS, PIE_CHART_HOVER_BACKGROUND_COLORS } from '../../../utils/constants';
+import { incomeStatementRenderText } from '../../../utils/i18n/income-statement-render-text';
+import { formatCurrency, getPercentage, localizeDate } from '../../../utils/util-fns';
+/**
+ * dateRanges should have the format of a DateRangeDTO object. Optional name field, required startDate and endDate fields.
+ * accountData should be an array of objects. accountData[i] should correspond with dateRange[i].
+ * Objects within accountData should have the format {accountNames: Array<String>, amounts: Array<Number, totalExpenses: Number}.
+ * accountData.accountNames[i] should correspond with accountData.amounts[i].
+ */
+function ExpensesByAccountPieCharts({dateRanges, accountData, loading}) {
+    const appContext = React.useContext(PageSettings);
+
+    const [pieChartObjects, setPieChartObjects] = React.useState([]);
+
+    const [fontColor, setFontColor] = React.useState(getComputedStyle(document.documentElement).getPropertyValue('--base-text-color'));
+
+    React.useEffect(() => {
+        setFontColor(
+            appContext.colorScheme === 'dark'
+                ? getComputedStyle(document.documentElement).getPropertyValue('--dark-mode-text-color')
+                : getComputedStyle(document.documentElement).getPropertyValue('--base-text-color')
+        );
+    }, [appContext.colorScheme]) //detects color scheme and changes the color of the chart text accordingly
+
+    React.useEffect(() => {
+        let pieCharts = [];
+        accountData.forEach((accountDataObject) => {
+            let pieChart = {
+                data: {
+                    labels: accountDataObject.accountNames,
+                    datasets: [{
+                        data: accountDataObject.amounts,
+                        borderColor: PIE_CHART_BORDER_COLORS,
+                        borderWidth: 2,
+                        backgroundColor: PIE_CHART_BACKGROUND_COLORS,
+                        hoverBorderColor: PIE_CHART_BORDER_COLORS,
+                    }]
+                },
+                options: {
+                    cutout: "40%",
+                    maintainAspectRatio: false,
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false,
+                            labels: {
+                                color: fontColor
+                            }
+                        },
+                        tooltip: {
+                            callbacks: {
+                                label: (toolTipItem) => {
+                                    return ` ${toolTipItem.label}: ${formatCurrency(appContext.locale, appContext.currency, toolTipItem.raw)} (${getPercentage(toolTipItem.raw, accountDataObject.totalExpenses)}%)`
+                                },
+                            },
+                        },
+                    },
+                    layout: {
+                        padding: {
+                            x: 80,
+                        }
+                    }    
+                }
+            }
+            pieCharts.push(pieChart);
+        })
+        setPieChartObjects(pieCharts);
+    }, [accountData, fontColor]);
+
+    return(
+        <div className="d-flex justify-content-center">
+            {loading
+                ? <LoadingSpinner big />
+                : pieChartObjects.map((pieChart, i) => {
+                    return(
+                        <div key={i} className="mx-4 d-flex flex-column align-items-center">
+                            <div style={{height: "260px"}} >
+                                <Doughnut
+                                    key={i}
+                                    data={pieChart.data}
+                                    options={pieChart.options}
+                                />
+                            </div>
+                            <div className="my-3">
+                                <div className="font-weight-semibold text-center" key={i}>
+                                    {dateRanges[i].name === "Custom" || !dateRanges[i].name
+                                        ?   <>
+                                                <div>
+                                                    {incomeStatementRenderText[appContext.locale]["From:"] + " " + localizeDate(dateRanges[i].startDate)}
+                                                </div>
+                                                <div>
+                                                    {incomeStatementRenderText[appContext.locale]["To:"] + " " + localizeDate(dateRanges[i].endDate)}
+                                                </div>
+                                            </>
+                                        : dateRanges[i].name}
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default ExpensesByAccountPieCharts;
--- front_end/src/pages/reports/expenses-by-account-page-v2.js
+++ front_end/src/pages/reports/expenses-by-account-page-v2.js
@@ -8,6 +8,7 @@ import { incomeStatementReportText } from '../../utils/i18n/income-statement-rep
 import { reportTypeListText } from '../../utils/i18n/report-type-list-text';
 import { formatCurrency, getPercentage } from '../../utils/util-fns';
 import DateRangeControls from './components/date-range-controls';
+import ExpensesByAccountPieCharts from './components/expenses-by-account-pie-charts';
 import ExpensesByAccountReport from './components/expenses-by-account-report';
 
 function ExpensesByAccountPageV2() {
@@ -17,6 +18,9 @@ function ExpensesByAccountPageV2() {
     const [incomeExpenseReportDto, setIncomeExpenseReportDto] = React.useState();
     const [detailedView, setDetailedView] = React.useState(false);
     const toggleDetailedView = () => setDetailedView(!detailedView);
+
+    const [dateRanges, setDateRanges] = React.useState([]);
+    const [pieChartAccountData, setPieChartAccountData] = React.useState([]);
 
     const convertAmountsIntoCurrencyAndPercentage = (amountsArray, totalArray) => {
         let returnedArray = []
@@ -31,10 +35,31 @@ function ExpensesByAccountPageV2() {
         axios.post(`${API_BASE_URL}/organization/${appContext.currentOrganizationId}/reports/incomeExpenseReport`, datesToRequest)
             .then(response => {
                 let dto = response.data;
+                let accountData = [];
+                dto.dateRanges.forEach(dateRange => {
+                    accountData.push({
+                        accountNames: [],
+                        amounts: []
+                    })
+                })
                 dto.expenseAccounts.forEach(account => {
-                    account.amounts = convertAmountsIntoCurrencyAndPercentage(account.amounts, dto.totalExpenses)
+                    accountData.forEach(accountDataObject => {
+                        accountDataObject.accountNames.push(account.accountName);
+                    })
+                    account.amounts.forEach((amount, i) => {
+                        accountData[i].amounts.push(amount);
+                    })
+                    account.amounts = convertAmountsIntoCurrencyAndPercentage(account.amounts, dto.totalExpenses);
+                    account.children.forEach(child => {
+                        child.amounts = convertAmountsIntoCurrencyAndPercentage(child.amounts, dto.totalExpenses);
+                    })
+                })
+                dto.totalExpenses.forEach((totalExpenseNumber, i) => {
+                    accountData[i].totalExpenses = totalExpenseNumber
                 })
                 setIncomeExpenseReportDto(dto);
+                setDateRanges(dto.dateRanges);
+                setPieChartAccountData(accountData);
                 setLoading(false);
             })
             .catch(response => {
@@ -61,6 +86,12 @@ function ExpensesByAccountPageV2() {
                             defaultStartDate={params.startDate? params.startDate: null}
                             defaultEndDate={params.endDate? params.endDate: null}
                         />
+                        <ExpensesByAccountPieCharts
+                            dateRanges={dateRanges}
+                            accountData={pieChartAccountData}
+                            loading={loading}
+                        />
+                        <hr/>
                         <ExpensesByAccountReport
                             incomeExpenseReportDto={incomeExpenseReportDto}
                             detailedView={detailedView}
